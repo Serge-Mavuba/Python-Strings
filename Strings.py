5852af8607cd8ff0ead0a432a68fa3e3960ee20d
Serge Mavuba <dev.mavuba@gmail.com>
2021-04-05 23:53:03 -0400

    Initial commit

---
+++ Strings.py
@@ -0,0 +1,28 @@
+# You can create strings using double quotes ""
+print()
+message = "This a quick intro to Python Strings"
+print(message)
+
+# You can create string using single quotes ''
+print()
+NEWmessage = 'Lets begin'
+print(NEWmessage)
+print()
+
+# If you want to create a string which contains an apostrophe using single quotes;
+# i.e.
+# THISmessage = 'Let's begin' (try this after you run the program once)
+# This will yield a syntax error
+
+# You can escape the apostrophe by putting a backslash in front of it
+THISmessage = 'Let\'s begin' 
+print(THISmessage)
+print()
+
+# You can create more complicated strings (which contain apostrophes and quotation marks) 
+# using triple quotes """ """
+THATmessage = """One of my favorite lines from the Godfather is:
+"I'm going to make him an offer he cant't refuse."
+Do you know who said this?"""
+print(THATmessage)
+print()
